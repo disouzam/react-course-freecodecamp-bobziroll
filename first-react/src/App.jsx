f766aaf5e76f40b6c9cba75c015bee061f43c232
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-01 20:53:57 -0300

    First component created - main content

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MyAwesomeNavBar from "./components/MyAwesomeNavBar";
+import MainContent from "./components/MainContent";
 
 const App = () => {
   return (
     <div>
       <MyAwesomeNavBar />
+      <MainContent />
     </div>
   );
 };
