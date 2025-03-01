d765ade594cfffc9d9e2333909e62e4074d7bc81
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-01 20:48:16 -0300

    Created first component - MyAwesomeNavBar - with code entirely copied from Scrimba's course page

---
+++ first-react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MyAwesomeNavBar from "./components/MyAwesomeNavBar";
+
+const App = () => {
+  return (
+    <div>
+      <MyAwesomeNavBar />
+    </div>
+  );
+};
+
+export default App;
