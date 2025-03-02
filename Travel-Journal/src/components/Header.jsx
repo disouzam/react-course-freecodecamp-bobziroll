78ed4b4a4784308697352ec7defeb5a19df898b9
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-02 18:37:13 -0300

    Adjusted font-family and alternate text in Header component

--- Travel-Journal/src/components/Header.jsx
+++ Travel-Journal/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <img src="Globe.png" alt="travel" />
+      <img src="Globe.png" alt="globe-icon" />
       <span>my travel journal.</span>
     </header>
   );
