ccdc5a4aaa6a8a85534eb355b7d3bda3b43e7b8e
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-01 22:29:14 -0300

    Basic content of ReactFacts project

---
+++ ReactFacts/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <img src="src/assets/react-logo.png" width="40px"></img>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jodan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
