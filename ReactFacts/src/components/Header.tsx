59787338e912221d476562b4190d790d8161bd80
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-02 10:49:14 -0300

    Added a nav element to Header component

--- ReactFacts/src/components/Header.tsx
+++ ReactFacts/src/components/Header.tsx
@@ -4,6 +4,13 @@ const Header = () => {
   return (
     <header>
       <img src="src/assets/react-logo.png" width="40px" alt="React logo"></img>
+      <nav>
+        <ul>
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
     </header>
   );
 };
