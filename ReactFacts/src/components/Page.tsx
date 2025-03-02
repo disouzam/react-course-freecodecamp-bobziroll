50a43c30442c99d316d2bcd47b7848cd7e659409
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-01 22:48:20 -0300

    Added a custom component called Page

---
+++ ReactFacts/src/components/Page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <img src="src/assets/react-logo.png" width="40px" alt="React logo"></img>
+      <h1>Fun facts about React!</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jodan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Page;
