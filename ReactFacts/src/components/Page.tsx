cf932d90ef9cff05e89060ea019015bc58583951
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-02 10:16:40 -0300

    Modified Page component to match semantically the content at 1:20:35 timestamp of YouTube video
    - https://youtu.be/x4rFhThSX04?si=4yqHcVNhmgjAtQKC&t=4835

--- ReactFacts/src/components/Page.tsx
+++ ReactFacts/src/components/Page.tsx
@@ -3,15 +3,28 @@ import React from "react";
 const Page = () => {
   return (
     <div>
-      <img src="src/assets/react-logo.png" width="40px" alt="React logo"></img>
-      <h1>Fun facts about React!</h1>
-      <ul>
-        <li>Was first released in 2013</li>
-        <li>Was originally created by Jodan Walke</li>
-        <li>Has well over 100K stars on GitHub</li>
-        <li>Is maintained by Meta</li>
-        <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      <header>
+        <img
+          src="src/assets/react-logo.png"
+          width="40px"
+          alt="React logo"
+        ></img>
+      </header>
+      <main>
+        <h1>Fun facts about React!</h1>
+        <ul>
+          <li>Was first released in 2013</li>
+          <li>Was originally created by Jodan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is maintained by Meta</li>
+          <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+      </main>
+      <footer>
+        <small>
+          &copy; 2024 Dickson Souza development. All rights reserved
+        </small>
+      </footer>
     </div>
   );
 };
