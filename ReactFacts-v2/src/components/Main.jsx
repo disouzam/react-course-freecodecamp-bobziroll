379cb4a3eaeafbbf0cf2ba65a73b0bc8b03ac433
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-02 16:40:20 -0300

    Basic configuration of Main component but formatting / styling of React logo in background is still pending

--- ReactFacts-v2/src/components/Main.jsx
+++ ReactFacts-v2/src/components/Main.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Main = () => {
-  return <h1>Main goes here</h1>;
+  return (
+    <main>
+      <h1>Fun facts about React!</h1>
+      <ul className="unordered-list">
+        <li className="unordered-list-item">Was first released in 2013</li>
+        <li className="unordered-list-item">
+          Was originally created by Jodan Walke
+        </li>
+        <li className="unordered-list-item">
+          Has well over 100K stars on GitHub
+        </li>
+        <li className="unordered-list-item">Is maintained by Meta</li>
+        <li className="unordered-list-item">
+          Powers thousands of enterprise apps, including mobile apps
+        </li>
+      </ul>
+      {/* <img
+        src="/react-logo.png"
+        className="main-background-logo"
+        alt="React logo"
+      ></img> */}
+    </main>
+  );
 };
 
 export default Main;
