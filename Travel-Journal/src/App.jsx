5edf02067adbcf7809b10922eefa82b51568bc4c
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-02 18:30:17 -0300

    Added a new Header component and configured its style

---
+++ Travel-Journal/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <h1>Here is the app content to be filled in</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ Travel-Journal/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="Globe.png" alt="travel" />
+      <span>my travel journal.</span>
+    </header>
+  );
+};
+
+export default Header;
