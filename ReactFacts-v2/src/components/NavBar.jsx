fb19a73642bcc1607dd73e8f7ab34c4cd804dfa7
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-02 13:28:35 -0300

    Nav bar stylized

--- ReactFacts-v2/src/components/NavBar.jsx
+++ ReactFacts-v2/src/components/NavBar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Navbar = () => {
-  return <h1>Navbar goes here</h1>;
+  return (
+    <>
+      <header className="header">
+        <nav className="nav">
+          <img
+            src="src/assets/react-logo.png"
+            className="nav-logo"
+            alt="React logo"
+          ></img>
+          <span className="nav-span">ReactFacts</span>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default Navbar;
