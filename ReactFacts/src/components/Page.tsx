72ab7878fbd57718f4e5bc677962fd15bd95b570
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-02 10:41:57 -0300

    Extracted MainContent and Footer from Page component

--- ReactFacts/src/components/Page.tsx
+++ ReactFacts/src/components/Page.tsx
@@ -1,25 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import MainContent from "./MainContent";
+import Footer from "./Footer";
 
 const Page = () => {
   return (
     <>
       <Header />
-      <main>
-        <h1>Fun facts about React!</h1>
-        <ul>
-          <li>Was first released in 2013</li>
-          <li>Was originally created by Jodan Walke</li>
-          <li>Has well over 100K stars on GitHub</li>
-          <li>Is maintained by Meta</li>
-          <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-      </main>
-      <footer>
-        <small>
-          &copy; 2024 Dickson Souza development. All rights reserved
-        </small>
-      </footer>
+      <MainContent />
+      <Footer />
     </>
   );
 };
