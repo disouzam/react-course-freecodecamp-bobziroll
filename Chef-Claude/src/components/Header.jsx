9f3585f7d2247ba1f0028fbff08b9e68f9838363
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-03 14:03:26 -0300

    Created Header component and add Styling to it

---
+++ Chef-Claude/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="chef-claude-icon.png" alt="Chef Claude" />
+      <h1>Chef Claude</h1>
+    </header>
+  );
+};
+
+export default Header;
