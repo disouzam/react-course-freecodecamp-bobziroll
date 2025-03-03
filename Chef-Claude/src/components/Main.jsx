abdd533d5af6cdb08eae98e3a5b8bfa802b6378c
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-03 14:45:01 -0300

    Dummy events in button to show how to implement simple event handlers

--- Chef-Claude/src/components/Main.jsx
+++ Chef-Claude/src/components/Main.jsx
@@ -5,6 +5,14 @@ const Main = () => {
     console.log("Clicked!");
   };
 
+  const handleMouseEnter = () => {
+    console.log(`Mouse entered at ${new Date()}`);
+  };
+
+  const handleMouseLeave = () => {
+    console.log(`Mouse left at ${new Date()}`);
+  };
+
   return (
     <main>
       <form className="add-ingredient-form">
@@ -13,7 +21,13 @@ const Main = () => {
           placeholder="e.g. oregano"
           aria-label="Add ingredient"
         />
-        <button onClick={handleClick}>Add ingredient</button>
+        <button
+          onClick={handleClick}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          Add ingredient
+        </button>
       </form>
     </main>
   );
