08043aad63949135a87d5c5bd05b7e545f3da6c9
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-04 16:03:05 -0300

    Implemented event handling to update top and bottom texts over meme

--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -7,17 +7,35 @@ export default function Main() {
     imageUrl: "http://i.imgflip.com/1bij.jpg",
   });
 
+  function handleChange(event) {
+    const { value, name } = event.currentTarget;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <main>
       <div className="form">
         <label>
           Top Text
-          <input type="text" placeholder="One does not simply" name="topText" />
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            onChange={handleChange}
+          />
         </label>
 
         <label>
           Bottom Text
-          <input type="text" placeholder="Walk into Mordor" name="bottomText" />
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            onChange={handleChange}
+          />
         </label>
         <button>Get a new meme image 🖼</button>
       </div>
