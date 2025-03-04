3c7b24ad512eb0943b79b86733d4d6dc0a7ea6cd
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-03-04 16:12:21 -0300

    Transformed input into controlled component

--- meme-generator/src/components/Main.jsx
+++ meme-generator/src/components/Main.jsx
@@ -25,6 +25,7 @@ export default function Main() {
             placeholder="One does not simply"
             name="topText"
             onChange={handleChange}
+            value={meme.topText}
           />
         </label>
 
@@ -35,6 +36,7 @@ export default function Main() {
             placeholder="Walk into Mordor"
             name="bottomText"
             onChange={handleChange}
+            value={meme.bottomText}
           />
         </label>
         <button>Get a new meme image 🖼</button>
